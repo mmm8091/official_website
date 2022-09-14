3c64162d671ba1903ab875b8f88707bf066e4de8
wofbi1 <liu@wofbi1.cn>
2022-09-14 11:57:29 +0800

    更新官网大量内容,包括档案的FAQ修改,捐款,修正大量不合适的文字

---
+++ docs/archives/project.md
@@ -0,0 +1,13 @@
+---
+title: 项目列表
+---
+#  项目列表
+
+<iframe  
+height=850
+width=100%
+src="https://aa5598an1s.feishu.cn/wiki/wikcnzdMOGWIg0Vju3WHxfxH16g"  
+frameborder=0  
+allowfullscreen>
+</iframe>
+
--- docs/archives/sidebar_contents.js
+++ docs/archives/sidebar_contents.js
@@ -1,12 +1,15 @@
 module.exports = [
 	'/archives/',
+	'plan',
+	'project',
+	'join_work',
+	'sponsor',
+	'faq',
 	{
-		title: "预览",
+		title: "预览(非正式内容)",
 		collapsable: true,
 		children: [
 			"outline_baibaiheyuzhen"
 		]
 	},
-	'faq',
-	'sponsor'
 ];
